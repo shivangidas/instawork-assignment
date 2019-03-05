43c741fb8309b0e61d852183af0ad7cc1cbb6509
shivangi <shivangi.das7@gmail.com>
2019-03-05 21:44:23 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Team List</h1>
+          <h2>count</h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
